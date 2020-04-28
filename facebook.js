f07a27ba5b9588ad3700367964ff61d48a7fc49d
nguyenphuccontact <64454328+nguyenphuccontact@users.noreply.github.com>
2020-04-28 12:39:32 +0700

    facebook.js

--- facebook.js
+++ facebook.js
@@ -49,8 +49,7 @@ function Facebook(accessToken, groupId)
             '\n' +
             '## thông tin\n' +
             '* Bài viết được tự động đăng lúc 12 giờ trưa hàng ngày\n' +
-            '* Đây là post tự động được tạo bởi Lerni và lấy dữ liệu từ VnExpress qua RSS các bạn có thể check source code [tại đây](https://bit.ly/lerni-bot-fb)');
-        formPublish.append('formatting', 'MARKDOWN');
+            '* Đây là post tự động được tạo bởi Lerni và lấy dữ liệu từ VnExpress qua RSS ;
 
         for (let i = 0; i < images.length ; i++) {
             let responseId = await publishImage(images[i]);
